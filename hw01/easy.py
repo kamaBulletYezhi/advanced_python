27f997d3624ad74ba64c774eb0f803518e4494ef
kamaBulletYezhi <kdmazitov@edu.hse.ru>
2022-02-12 15:50:10 +0300

    easy done

--- hw01/easy.py
+++ hw01/easy.py
@@ -2,12 +2,12 @@ def fibonacci(n: int):
     if type(n) is not int:
         print(f'{int(n)} is used')
         n = int(n)
-    if n < 0:
-        raise ValueError('n < 0')
-    if n < 2:
-        return n
+    if n <= 0:
+        raise ValueError('n <= 0')
     prev = 0
     curr = 1
+    yield curr
     for i in range(1, n):
         prev, curr = curr, prev+curr
-    return curr
+        yield curr
+
