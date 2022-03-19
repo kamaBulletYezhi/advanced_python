ad0a472321cee0eecc313a8fbefdc4fbeca98710
kamaBulletYezhi <kdmazitov@edu.hse.ru>
2022-03-19 16:01:54 +0300

    Внесены исправление в hw05/easy.py

--- hw05/easy.py
+++ hw05/easy.py
@@ -1,20 +1,23 @@
 import asyncio
 import aiohttp
+import aiofiles
 import sys
 
 URL = "https://picsum.photos/500"
+path = sys.argv[2] if len(sys.argv) > 2 else "artifacts/easy/"
+
+
+async def sub_task(session, img_num):
+    async with session.get(URL) as response:
+        async with aiofiles.open(f"{path}{img_num}.png", "bw") as file:
+            await file.write(await response.read())
 
 
 async def run():
     count = int(sys.argv[1])
-    path = sys.argv[2] if len(sys.argv) > 2 else "artifacts/easy/"
-
     async with aiohttp.ClientSession() as session:
-        for i in range(count):
-            async with session.get(URL) as response:
-                with open(f"{path}{i}.png", "bw") as file:
-                    file.write((await response.content.read()))
+        tasks = (sub_task(session, i) for i in range(count))
+        await asyncio.gather(*tasks)
 
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(run())
+asyncio.run(run())
